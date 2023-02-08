74ac4f5b3731476c0eb11741a661be3a0c3a8c1c
subhodeep2002 <90915342+ronjackson007@users.noreply.github.com>
2023-02-08 15:25:33 +0530

    Lab_4 Added

--- Laab_4.py
+++ Laab_4.py
@@ -2,4 +2,26 @@
 a = 20
 b = 34
 if b > a: print("b is greater")
+# Short Hand If ..... Else .....
+a = 2
+b = 34
+print("a") if a > b else print("b is greater")
+
+# One Line if else statements
+a = 330
+b = 330
+print("a") if a > b else print("=") if a == b else print("b")
+
+# And
+# the and keyword is a logical operator, and is used to
+
+a = 30
+b = 3
+c = 30
+
+if a>b and c > b:
+    print("a and c are greater than b")
+else:
+    print("c is greater")
+
 
