6b1be1733d7be16609bb792fb49c36ad2669fb83
subhodeep2002 <90915342+ronjackson007@users.noreply.github.com>
2023-03-14 15:24:46 +0530

    EXP6 ADDED

---
+++ EXP6.py
@@ -0,0 +1,32 @@
+#Create a tuple and perform the following operations:
+#1. Add Items
+#2. length
+#3. Check for item in tuple
+#4. Access items
+
+t = ("Apple", "banana", "cherry", "orange")
+t2 = ('mango',)
+print(t)
+
+# 1. Add Items
+
+# t[4] = 'mango'
+# TypeError: 'tuple' object does not support item assignment
+t1 = t + t2
+print(t1)
+
+# 2. length
+print(len(t))
+
+# 3. Check for item in tuple
+a = 'Apple'
+if a in t:
+    print("It is present in tuple")
+else:
+    print("It is not present in tuple")
+
+# 4. Access items
+print(t[0])
+print(t[1])
+for i in range(0, 3):
+    print(t[i])
