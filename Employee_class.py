691000ed996668694f0cd1eb70ea62ee7d42bd38
subhodeep2002 <90915342+ronjackson007@users.noreply.github.com>
2023-03-24 12:29:51 +0530

    EXP9-   >NEW   ADDED

---
+++ Employee_class.py
@@ -0,0 +1,25 @@
+class Employee(object):
+    # pass
+    no_of_employee = 100
+    def __init__(self, name, salary, pos):
+        self.name = name
+        self.salary = salary
+        self.pos = pos
+    def display(self):
+        print(self.name, self.salary, self.pos)
+
+
+res1 = Employee('raju', 40000, 'ccc')
+res2 = Employee('Arjun', 10000, 'cdd')
+
+res1.display()
+res2.display()
+
+# r = Employee()
+# s = Employee()
+# r.name = 'raju'
+# r.salary = 40000
+# s.salary = 99000
+# s.name = 'arjun'
+# print(r.name, r.salary, s.name, s.salary)
+
